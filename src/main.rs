a03a64fb1c36b5e84bd3c2c9b4422f0b4b678e46
Manpreet Singh <msp99000@gmail.com>
2023-07-07 00:41:21 +0530

    Modified files

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn main() {
         eprintln!("Usage: `source` `target`");
         return;
     }
-    
+    // Define input and output
     let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
     let output = File::create(args().nth(2).unwrap()).unwrap();
     let mut encoder = GzEncoder::new(output, Compression::default());
