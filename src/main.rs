7f5cb544d230dbb1663af0635ac0a9a0be397a9f
Manpreet Singh <msp99000@gmail.com>
2023-09-30 13:39:08 +0530

    chore: adds script to load BufRead

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use std::io::copy;
 use std::io::BufReader;
 use std::time::Instant;
 
-fn main() {
+n main() {
     if args().len() != 3 {
         eprintln!("Usage: `source` `target`");
         return;
